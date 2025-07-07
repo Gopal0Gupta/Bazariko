057dbaa73777b97e63adea76902248e47c32ebe4
Gopal Gupta <k7523337@gmail.com>
2025-07-08 03:26:20 +0530

    Project Setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.gopal.bazariko"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.gopal.bazariko"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
